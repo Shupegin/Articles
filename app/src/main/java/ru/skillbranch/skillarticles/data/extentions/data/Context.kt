78546f755f899ac33f2ced26c2c803b4566b83f5
Дмитрий <Kalibri7024@gmail.com>
2022-11-26 18:15:00 +0500

    hometask_3

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/extentions/data/Context.kt
@@ -0,0 +1,41 @@
+package ru.skillbranch.skillarticles.extensions
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.TypedValue
+
+fun Context.dpToPx(dp: Int): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp.toFloat(),
+        this.resources.displayMetrics
+
+    )
+}
+
+fun Context.dpToIntPx(dp: Int): Int {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp.toFloat(),
+        this.resources.displayMetrics
+    ).toInt()
+}
+
+val Context.isNetworkAvailable: Boolean
+    @SuppressLint("MissingPermission")
+    get() {
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cm.activeNetwork?.run {
+                val nc = cm.getNetworkCapabilities(this)
+                nc!!.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || nc.hasTransport(
+                    NetworkCapabilities.TRANSPORT_WIFI
+                )
+            } ?: false
+        } else {
+            cm.activeNetworkInfo?.run { isConnectedOrConnecting } ?: false
+        }
+    }
